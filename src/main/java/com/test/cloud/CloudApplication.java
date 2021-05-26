533f27ff580d1b5b497ebb175180b810313f101d
Praveen Yadav <pk96375@gmail.com>
2021-05-26 12:29:25 +0530

    War Deployment

--- src/main/java/com/test/cloud/CloudApplication.java
+++ src/main/java/com/test/cloud/CloudApplication.java
@@ -2,9 +2,10 @@ package com.test.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CloudApplication {
+public class CloudApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CloudApplication.class, args);
