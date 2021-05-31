26d71e34435e587e4be5d72e7a07d2e5dbbd7629
Praveen Yadav <pk96375@gmail.com>
2021-05-31 14:03:02 +0530

    Parameterised GET request

--- src/main/java/com/test/cloud/CloudApplication.java
+++ src/main/java/com/test/cloud/CloudApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CloudApplication extends SpringBootServletInitializer {
+public class CloudApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CloudApplication.class, args);
--- src/main/java/com/test/cloud/controller/TestCloudController.java
+++ src/main/java/com/test/cloud/controller/TestCloudController.java
@@ -1,13 +1,14 @@
 package com.test.cloud.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestCloudController {
 
-	@GetMapping("/sayhello")
-	public String sayHelloToCloud() {
-		return "Hello Test Cloud - Openshift";
+	@GetMapping(value = "/sayhello")
+	public String sayHelloToCloud(@RequestParam("name") String name) {
+		return "Message from OpenShift clouds : Hello " + name;
 	}
 }
